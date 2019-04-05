5e039a750dbf5180a375436ec6026592b4b06c78
Nathan Rodrigo <43752250+nrodrigoc@users.noreply.github.com>
2019-04-05 00:53:47 -0300

    Realocação

--- ICG_T1/mygl.h
+++ ICG_T1/mygl.h
@@ -10,10 +10,9 @@
 struct Vertice{
         int x;
         int y;
-
 }typedef Vertice;
 
-//Cores dos pixeis
+//Cores dos pixels
 struct RGBA{
     int red;
     int green;
@@ -38,84 +37,116 @@ void putPixel(Vertice pixel, RGBA cor){
 
 //Em caso de retas verticais
 void noIncLine(Vertice p1, Vertice p2, RGBA c1, RGBA c2){
-    if(p2.y < p1.y){
-        noIncLine(p2, p1, c2, c1);
-        return;
-    }
-    while(p1.y != p2.y){
-        putPixel(p1, c1);
-        p1.y++;
+    putPixel(p1, c1);
+    if(p1.x == p2.x){
+        if(p2.y < p1.y){
+            noIncLine(p2, p1, c2, c1);
+            return;
+        }
+        while(p1.y != p2.y){
+            putPixel(p1, c1);
+            p1.y++;
+        }
+    }else{
+        if(p2.x < p1.x){
+            noIncLine(p2, p1, c2, c1);
+            return;
+        }
+        while(p1.x != p2.x){
+            putPixel(p1, c1);
+            p1.x++;
+        }
     }
 }
 
 
 //Desenho de retas inclinadas
-void drawLine(Vertice pixel1, Vertice pixel2, RGBA cor1, RGBA cor2){
+void drawLine(Vertice pixel1, Vertice pixel2, RGBA cor){
 
 
     //delta x e delta y
     int dx = pixel2.x - pixel1.x;
     int dy = pixel2.y - pixel1.y;
-    int inclinacao = 0;
+    int inclinacao = 1;
 
-    /*inverte os vertices caso o dx seja negativo*/
-    if(dx<0){
-        drawLine(pixel2,pixel1,cor2,cor1);
+    if(dx == 0 || dy == 0){
+        noIncLine(pixel1, pixel2, cor, cor);
         return;
     }
 
-    if(dx == 0){
-        noIncLine(pixel1, pixel2, cor1, cor2);
+    if(dx < 0){
+        drawLine(pixel2, pixel1, cor);
         return;
     }
 
-    if(dy<0){
+    if(dy < 0){
         inclinacao = -1;
-    }else{
-        inclinacao = 1;
     }
 
+    int d;                       //variavel de decisao
 
-    int d;
-
-    RGBA cor = cor1;
     Vertice pixel = pixel1;
 
-    putPixel(pixel, cor);
+    putPixel(pixel, cor);     //Raster do primeiro pixel
 
-    if(dx >= inclinacao*dy){    // inclinacao maior ou igual a 1
-        if(dy<0){ // caso y2 < y1
-            d = 2*dy+dx;
+    if(dx >= inclinacao*dy){
+        if(dy < 0){
+            d = 2 * dy + dx;
             while(pixel.x < pixel2.x){
-                if(d<0){// escolhido é o pixel acima do ponto medio
+                if(d < 0){
                     d += 2*(dy+dx);
                     pixel.x++;
                     pixel.y--;
-
-                }else{//escolhido é o pixel abaixo do ponto medio
-                    d+=2*dy;
-                    pixel.x++; // varia apenas no eixo x
-
+                }else{
+                    d += 2*dy;
+                    pixel.x++;
                 }
-                putPixel(pixel,cor);
+                putPixel(pixel, cor);
             }
-        }else{ // caso y1<y2
-            d=2*dy-dx;
-            while(pixel.x<pixel2.x){
-                if(d<0){ //o pixel de cima é escolhido
-                    d+=2*dy;
-                    pixel.x++; // varia apenas no eixo x
-                }
-                else{ //o pixel de baixo é escolhido
-                    d+=2*(dy-dx);
+        }else{
+            d = 2 * dy - dx;
+            while(pixel.x < pixel2.x){
+                if(d < 0){                //Pixel abaixo do ponto medio escolhido
+                    d += 2 * dy;
+                    pixel.x++;
+                }else{
+                    d += 2 * (dy - dx);   //Pixel acima do ponto medio escolhido
                     pixel.x++;
                     pixel.y++;
                 }
-                putPixel(pixel,cor);
+                putPixel(pixel, cor);
+            }
+        }
+    }else{
+        if(dy < 0){
+            d = dy + 2*dx;
+            while(pixel.y > pixel2.y){
+                if(d < 0){
+                    d += 2 * dx;
+                    pixel.y--;
+                }else{
+                    d += 2 * (dy + dx);
+                    pixel.x++;
+                    pixel.y--;
+                }
+                putPixel(pixel, cor);
+            }
+        }else{ // y1 < y2
+            d = dy - 2*dx;
+            while(pixel.y < pixel2.y){
+                if(d < 0){
+                    d += 2 * (dy - dx);
+                    pixel.x++;
+                    pixel.y++;
+                }else{
+                    d += -2 * dx;
+                    pixel.y++;
+                }
+                putPixel(pixel, cor);
             }
         }
     }
-    putPixel(pixel2, cor2);
+    putPixel(pixel2, cor);
 }
 
 void desenhar(){
@@ -125,19 +156,18 @@ void desenhar(){
     RGBA cor = {255, 0, 0, 255};
 
     //triangulo1
-    drawLine(p1, p2, cor, cor);
-    drawLine(p1, p3, cor, cor);
-    drawLine(p2, p3, cor, cor);
+    drawLine(p1, p2, cor);
+    drawLine(p1, p3, cor);
+    drawLine(p2, p3, cor);
 
     //triangulo2
     Vertice p4 = {200, 300};
     Vertice p5 = {400, 200};
     Vertice p6 = {200, 250};
 
-    drawLine(p4, p5, cor, cor);
-    drawLine(p4, p6, cor, cor);
-    drawLine(p5, p6, cor, cor);
-
+    drawLine(p4, p5, cor);
+    drawLine(p4, p6, cor);
+    drawLine(p5, p6, cor);
 }
 
 
